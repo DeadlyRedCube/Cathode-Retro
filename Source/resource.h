0baa1cc5c7189852a427a4130c4699197a5e9997
DeadlyRedCube <josh@drilsej.com>
2022-02-12 01:04:42 -0800

    Remove CGA code for now
    It's bad and I can do better

--- Source/resource.h
+++ Source/resource.h
@@ -13,15 +13,13 @@
 #define IDR_GENERATE_SHADOW_MASK        109
 #define IDR_DOWNSAMPLE_2X               110
 #define IDR_RGB_TO_CRT                  111
-#define IDR_RGB_TO_CGA                  112
-#define IDR_CGA_TO_SVIDEO_OR_COMPOSITE  113
-#define IDR_GENERATE_PHASE_TEXTURE      114
+#define IDR_GENERATE_PHASE_TEXTURE      112
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        115
+#define _APS_NEXT_RESOURCE_VALUE        113
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
