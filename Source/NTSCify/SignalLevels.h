acae27b9e9a7a7c4dda944f019fac644a3b4ad78
DeadlyRedCube <josh@drilsej.com>
2023-08-23 00:21:01 -0700

    Add a comment to SignalLevels::isDoubled because I kept forgetting what it was

--- Source/NTSCify/SignalLevels.h
+++ Source/NTSCify/SignalLevels.h
@@ -4,12 +4,13 @@ namespace NTSCify
 {
   struct SignalLevels
   {
-    bool isDoubled;
-
+    float temporalArtifactReduction;
+    bool isDoubled;       // This can be true if we're doing temporal aliasing reduction - the texture has 2 versions of this frame with 
+                          //  different temporal aliasing that can be blended. It's only true if this frame and the previous frame had
+                          //  different signal phases (otherwise there are no artifacts to reduce)
     float whiteLevel;
     float blackLevel;
     float saturationScale;
     
-    float temporalArtifactReduction;
   };
 }
