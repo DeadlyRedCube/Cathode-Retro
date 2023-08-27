96ac3faa99ebae3edea9810e5f82d5cdaa30e35c
DeadlyRedCube <josh@drilsej.com>
2023-08-27 02:41:28 -0700

    Half-optimize Downsample2X shader
    The lanczos filter is now done using half the taps as it was, thanks to linear filtering!
    
    The shader is still doing a 2D filter when it should really be 2 1D passes, but now at least it's 16 taps per pixel instead of 64, for the same result. Progress!

--- Shaders/Downsample2x.hlsl
+++ Shaders/Downsample2x.hlsl
@@ -1,30 +1,49 @@
+// Downsample an input image by 2x along each axis by using a lanczos filter.
+
+
+// This is a lanczos2 kernel for a nice 2x downsample. I wish I'd documented how I generated it but I didn't so instead this is what you
+//  get. Sorry bout that.
+//static const float k_lanczos2[8] = {-0.009, -0.042, 0.117, 0.434, 0.434, 0.117, -0.042, -0.009};
+
+// However, it's been optimized to take advantage of the linear filtering using the same technique I used for the gaussian filters at
+//  https://drilian.com/gaussian-kernel/
+// That means it's only 4 texture samples for an 8-tap lanczos instead of a full 8, meaning this dumb 8x8 filter is 16 taps instead of 
+//  64. Obviously this could be done in two 4-tap passes (horizontal then vertical) but this is done as a preprocess at the moment so
+//  I didn't bother.
+static const uint k_sampleCount = 4;
+static const float k_coeffs[4] = 
+{
+  -0.051,
+   0.551,
+   0.551,
+  -0.051,
+};
+
+static const float k_offsets[4] = 
+{
+  -2.67647052,
+  -0.712341249,
+   0.712341189,
+   2.67647052,
+};
+
+
 Texture2D<unorm float4> g_sourceTexture : register(t0);
-
-static const float k_lanczos2[8] = {-0.009, -0.042, 0.117, 0.434, 0.434, 0.117, -0.042, -0.009};
-
 sampler g_sampler : register(s0);
 
 
-// Downsample an input image by 2x along each axis by using a lanczos filter.
-// $TODO This could be done in half the taps (and also separably) to be more efficient, but since it's only done as a texture-generation process, I
-//  kinda don't care.
 float4 main(float2 inTexCoord: TEX): SV_TARGET
 {
   float2 inputTexelSize = float2(ddx(inTexCoord).x, ddy(inTexCoord).y) * 0.5;
   inTexCoord -= 0.5 * inputTexelSize;
 
-  uint2 dim;
+  float4 color = float4(0,0,0,0);
+  for (uint y = 0; y < k_sampleCount; y++)
   {
-    g_sourceTexture.GetDimensions(dim.x, dim.y);
-  }
-  float4 color = float4(0.0, 0.0, 0.0, 0.0);
-  for (int y = -3; y < 5; y++)
-  {
-    for (int x = -3; x < 5; x++)
+    for (uint x = 0; x < k_sampleCount; x++)
     {
-      float4 samp = g_sourceTexture.Sample(g_sampler, inTexCoord + float2(x, y) * inputTexelSize);
-      
-      color += samp * k_lanczos2[x + 3] * k_lanczos2[y + 3];
+      float4 samp = g_sourceTexture.Sample(g_sampler, inTexCoord + float2(k_offsets[x], k_offsets[y]) * inputTexelSize);
+      color += samp * k_coeffs[x] * k_coeffs[y];
     }
   }
 
