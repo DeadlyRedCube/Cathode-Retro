27cdc38c1489ce459d513821ed277c08b4dceafe
DeadlyRedCube <josh@drilsej.com>
2023-11-20 02:56:58 -0800

    Doc formatting and iOS Safari scrolling fixes
    - Changed the way the sidebar scrolls the selected element into view - it now overrides scrollability on html and body and puts the scrollbar, instead, on the outer list of the menu instead, then sets that scroll position directly (and does a better job than before)
    - When the sidebar is open, there's now a darkening element that blocks clicking on the root page (and main page scrolling is disabled)
    - Changed the code font and weight
    - Added narrow-screen lr-table formatting to be ordered instead of side by side for better use of screen width real estate.

--- docs/main-scripts.js
+++ docs/main-scripts.js
@@ -2,9 +2,15 @@ function ToggleMenu()
 {
   let sidebar = document.getElementById("sidebar-container");
   if (sidebar.classList.contains("opened"))
-    { sidebar.classList.remove("opened"); }
+  {
+    sidebar.classList.remove("opened"); 
+    document.body.classList.remove("sidebar-opened");
+  }
   else
-    { sidebar.classList.add("opened"); }
+  { 
+    sidebar.classList.add("opened"); 
+    document.body.classList.add("sidebar-opened");
+  }
 }
 
 function OnLoad()
@@ -12,4 +18,12 @@ function OnLoad()
   // Do whatever needs to be done at page load time // https://docs.sonata-project.org/projects/SonataAdminBundle/en/4.x/reference/action_show/
   let e = document.getElementById("sidebar-button");
   e.onclick = ToggleMenu;
+
+  // On safari hitting "back" can re-show the page as it was, which means we need to explicitly close the sidebar if it was still open.
+  document.body.onpageshow = () =>
+  {
+    let sidebar = document.getElementById("sidebar-container");
+    sidebar.classList.remove("opened");
+    document.body.classList.remove("sidebar-opened");
+  };
 }
