e89b3eb6bfac5e9e9d99dd4de7065f99afdbb63a
DeadlyRedCube <josh@drilsej.com>
2023-11-29 21:34:27 -0800

    Break up all C++ code lines longer than 120 characters
    I'd made a change before release to make 120 be the maximum line length, but apparently missed a few files.
    This change also eliminates the two "#pragma comment" lines (mostly because the linker one was massive, moving that information instead into the vcxproj files.

--- Samples/Common/SettingsDialog.cpp
+++ Samples/Common/SettingsDialog.cpp
@@ -9,8 +9,6 @@
 #include "resource.h"
 #include "SettingsDialog.h"
 
-#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#pragma comment(lib, "comctl32.lib")
 
 template <typename T>
 constexpr auto EnumValue(T v)
